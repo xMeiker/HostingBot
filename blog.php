7421c9eab6aecf803eab719169127f9d04f251b8
xMeiker <89752513+xMeiker@users.noreply.github.com>
2024-05-17 01:12:59 -0600

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Blog de Viajes</title>
+
+      <!-- enlace css deslizador  -->
+      <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+      <!-- fuente impresionante enlace cdn  -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+      <!-- enlace de archivo css personalizado  -->
+      <link rel="stylesheet" href="css/style.css">
+
+      <!-- visualizar favicon para la pagina web desde distintos dispositivos -->
+      <link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+         <link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+         <link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+         <link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+         <link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+         <link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+         <link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+         <link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+         <link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
+         <link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
+         <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+         <link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+         <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+         <link rel="manifest" href="/manifest.json">
+         <meta name="msapplication-TileColor" content="#ffffff">
+         <meta name="msapplication-TileImage" content="images/ms-icon-144x144.png">
+         <meta name="theme-color" content="#ffffff">
+         
+
+   </head>
+   <body>
+      
+      <!-- comienza la sección del encabezado  -->
+
+      <section class="header">
+
+         <!-- Enlace con la imagen PNG como logo -->
+         <a href="home.php" class="logo">
+            <!-- Imagen PNG -->
+            <img src="images/alamoicon.png" alt="AlamoIcon">
+         </a>
+
+         <div class="menu">
+            <img src="images/alamomenu.png" alt="AlamoMenu">
+            <div class="menu-content">
+                  <a href="home.php" class="menu-item">Inicio</a>
+                  <a href="directory.php" class="menu-item">Directorio</a>
+                  <a href="activities.php" class="menu-item">Actividades</a>
+                  <a href="blog.php" class="menu-item">Blog de Viajes</a>
+                  <a href="descuentos.php" class="menu-item">Ofertas y Descuentos</a>
+                  <a href="quests.php" class="menu-item">Recomendaciones</a>
+            </div>
+         </div>
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </section>
+
+      <!-- termina la sección del encabezado -->
+
+      <div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+         <h1>Blog de Viajes</h1>
+      </div>
+
+      <!-- empieza la seccion de about 1  -->
+
+      <section class="about">
+
+         <div class="image">
+            <img src="images/image1.png" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Esto es lo mejor!</h3>
+            <p>Descubrir los encantos de Tijuana nunca fue tan fácil. Gracias a esta página web dedicada a los lugares turísticos de la ciudad, ahora podemos acceder rápidamente a una amplia gama de información actualizada. Desde atracciones icónicas hasta gemas ocultas, todo está al alcance de nuestros dedos las 24 horas del día. Además, esta herramienta nos permite personalizar nuestra experiencia de viaje según nuestros intereses y preferencias, garantizando que cada momento sea único y memorable.</p>
+
+            <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+         </div>
+
+      </section>
+
+     
+<section class="about">
+
+<div class="content">
+   <h3>Cuidado con el clima</h3>
+   <p>Los próximos días en Tijuana se vislumbran con un mal clima en el horizonte. Los meteorólogos advierten que una serie de sistemas climáticos convergentes traerán consigo fuertes lluvias y vientos intensos a la región. Se espera que las nubes grises dominen el cielo mientras la lluvia cae persistente, inundando calles y desafiando a los transeúntes con charcos esquivos. Además, el viento soplará con fuerza, sacudiendo árboles y carteles publicitarios mientras deja una sensación de frescura inusual en el aire. Los residentes y visitantes de Tijuana están siendo exhortados a prepararse adecuadamente y tomar precauciones adicionales ante este inminente clima adverso. ¡Es hora de sacar los paraguas y abrigarse bien!</p>
+   <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+   </div>
+
+<div class="image">
+<img src="images/image2.png" alt="">
+</div>
+
+</section>
+<section class="about">
+
+         <div class="image">
+         <img src="images/image3.png" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Consejos para la playa</h3>
+            <p>Antes de sumergirse en las olas y disfrutar del sol radiante en la playa, es esencial tomar algunas precauciones para protegerse del intenso calor y los rayos UV. En primer lugar, no olvides aplicar generosamente protector solar de amplio espectro con un SPF de al menos 30, especialmente en áreas expuestas como la cara, los brazos y la espalda. Reaplica el protector solar cada dos horas, especialmente después de nadar o sudar. Además, opta por sombrillas o gorras de ala ancha para proteger tu piel del sol directo y busca la sombra durante las horas pico de radiación solar, generalmente entre las 10 a.m. y las 4 p.m. No te olvides de hidratarte constantemente bebiendo agua y evitando el exceso de alcohol y cafeína, que pueden deshidratarte. Finalmente, asegúrate de llevar contigo una bolsa de playa con elementos esenciales como toallas, gafas de sol, una botella de agua reutilizable, aperitivos saludables y, por supuesto, ¡mucha actitud positiva para disfrutar al máximo de tu día en la playa!</p>
+            <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+           
+         </div>
+
+      </section>
+      <section class="about">
+
+<div class="content">
+   <h3>Los Sabores de Tijuana: Explorando la Escena Gastronómica Local</h3>
+   <p>Descubre los secretos culinarios que Tijuana tiene para ofrecer. Desde los tacos de pescado fresco hasta la deliciosa birria, la ciudad es un paraíso para los amantes de la comida. No te pierdas la oportunidad de probar las innovadoras fusiones de la cocina Baja Med, que combinan ingredientes locales con técnicas culinarias internacionales. Además, no olvides explorar los vibrantes mercados y puestos callejeros donde puedes deleitarte con auténticos antojitos mexicanos. ¡Prepárate para un festín de sabores en Tijuana!</p>
+   <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+   </div>
+
+<div class="image">
+<img src="images/image4.png" alt="">
+</div>
+
+</section>
+<section class="about">
+
+         <div class="image">
+         <img src="images/image5.png" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Explorando su Escena Cultural Vibrante</h3>
+            <p>Sumérgete en el rico mundo del arte y la cultura en Tijuana. Desde galerías de arte contemporáneo hasta coloridos murales callejeros, la ciudad ofrece una experiencia cultural única. No te pierdas la oportunidad de visitar el Centro Cultural Tijuana (CECUT) con su icónico domo IMAX y su impresionante colección de arte. Además, explora el dinámico distrito cultural de Zona Río, donde encontrarás teatros, museos y espacios de exhibición. ¡Descubre el alma creativa de Tijuana en cada rincón!</p>
+            <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+           
+         </div>
+
+      </section>
+      <section class="about">
+
+<div class="content">
+   <h3>Descubriendo los Tesoros Naturales de Tijuana</h3>
+   <p>Embárcate en una emocionante aventura al aire libre en Tijuana. Desde majestuosas montañas hasta impresionantes playas, la ciudad ofrece una variedad de actividades para los amantes de la naturaleza. Explora el Parque Nacional Constitución de 1857, donde encontrarás senderos para caminatas, cascadas y espectaculares vistas panorámicas. Además, no te pierdas la oportunidad de practicar surf en las playas de Rosarito o hacer una excursión de observación de ballenas en el océano Pacífico. ¡Descubre la belleza natural de Tijuana y sumérgete en la aventura!</p>
+   <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+</div>
+
+
+<div class="image">
+<img src="images/image6.png" alt="">
+</div>
+
+</section>
+<section class="about">
+
+         <div class="image">
+         <img src="images/image7.png" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Centros Comerciales y Distritos de Tijuana</h3>
+            <p>Sumérgete en la emoción de las compras y el entretenimiento en Tijuana. Visita los modernos centros comerciales como Plaza Río y Macroplaza, donde encontrarás una amplia variedad de tiendas, restaurantes y opciones de entretenimiento para toda la familia. Además, explora el colorido distrito de moda en la Zona Centro, donde encontrarás boutiques únicas y mercados callejeros llenos de artesanías locales. No te pierdas la oportunidad de disfrutar de la vibrante vida nocturna de la ciudad, con una amplia selección de bares, clubes y eventos culturales. ¡Prepárate para una experiencia de compras y entretenimiento inolvidable en Tijuana!</p>
+
+            <div class="date">
+        <p>Fecha: 26 de abril de 2024</p>
+    </div>
+         </div>
+
+      </section>
+         
+
+<!-- Termina la sección de about -->
+
+
+    
+
+      <!-- empieza el pie de pagina  -->
+
+      <section class="footer">
+
+         <div class="box-container">
+
+            <div class="box">
+               <h3>Enlaces</h3>
+               <a href="home.php"> <i class="fas fa-angle-right"></i> inicio</a>
+               <a href="about.php"> <i class="fas fa-angle-right"></i> Acerca De </a>
+               <a href="package.php"> <i class="fas fa-angle-right"></i> Paquetes </a>
+               <a href="book.php"> <i class="fas fa-angle-right"></i> Reserva</a>
+            </div>
+
+            <div class="box">
+               <h3>Sobre Nosotros</h3>
+               <a href="#"> <i class="fas fa-angle-right"></i> preguntas</a>
+               <a href="#"> <i class="fas fa-angle-right"></i> acerca de</a>
+               <a href="#"> <i class="fas fa-angle-right"></i> politica de privacidad</a>
+               <a href="#"> <i class="fas fa-angle-right"></i> terminos de uso</a>
+            </div>
+
+            <div class="box">
+               <h3>contact info</h3>
+               <a href="#"> <i class="fas fa-phone"></i> 6642149981 </a>
+               <a href="#"> <i class="fas fa-phone"></i> 6648738493 </a>
+               <a href="#"> <i class="fas fa-envelope"></i> omar.garcia201@tectijuana.edu.mx </a>
+               <a href="#"> <i class="fas fa-map"></i> Tijuana, Baja californa, Mexico</a>
+            </div>
+
+            <div class="box">
+               <h3>follow us</h3>
+               <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+               <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+               <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+               <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            </div>
+
+         </div>
+
+         <div class="credit"> creado por <span>Private Studio</span> | ¡Todos los derechos reservados! </div>
+
+      </section>
+      <!-- termina el pie de pagina -->
+
+      <!-- enlace js deslizador  -->
+      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+      <!-- enlace de archivo js personalizado  -->
+      <script src="js/script.js"></script>
+
+   </body>
+</html>
